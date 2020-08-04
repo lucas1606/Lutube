465d3ef327aa7a3b897295fa3a533a8a66bcd904
lucas1606 <lucas.almeida1606@gmail.com>
2020-08-04 13:33:59 -0300

    Update

--- lutube/src/pages/Home/index.js
+++ lutube/src/pages/Home/index.js
@@ -3,32 +3,31 @@ import Menu from '../../components/menu';
 import Footer from '../../components/Footer';
 import Carousel from '../../components/Carousel';
 import dadosIniciais from '../../data/dados_iniciais.json';
-import BannerMain from '../../components/BannerMain'
+import BannerMain from '../../components/BannerMain';
 
 function Home() {
-
-  
   return (
-    
-    
-    <div style={{background:'#141414'}}>
-       <Menu />
-       <BannerMain videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-  videoDescription={dadosIniciais.categorias[0].videos[0].titulo}
-  url= {dadosIniciais.categorias[0].videos[0].url} />;
 
-        <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
-        <Carousel ignoreFirstVideo category={dadosIniciais.categorias[1]} />
-        <Carousel ignoreFirstVideo category={dadosIniciais.categorias[2]} />
-        <Carousel ignoreFirstVideo category={dadosIniciais.categorias[3]} />
-        <Carousel ignoreFirstVideo category={dadosIniciais.categorias[4]} />
-        <Carousel ignoreFirstVideo category={dadosIniciais.categorias[5]} />
+    <div style={{ background: '#141414' }}>
+      <Menu />
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        videoDescription={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+      />
+      ;
 
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[1]} />
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[2]} />
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[3]} />
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[4]} />
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[5]} />
 
-       <Footer />
-       </div>
-  
+      <Footer />
+    </div>
+
   );
-};
+}
 
 export default Home;
